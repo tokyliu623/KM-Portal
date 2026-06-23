9c048d8ebb05db021d2137fc122a194ae645f2f9
tokyliu623 <469753809@qq.com>
2026-06-23 10:34:11 +0800

    chore: bump version to 1.0.4 and add release package

---
+++ 【03】Workspace/【02】内容知识库专项/km-operation-builder/scripts/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""km-operation-builder scripts package."""
+
+__version__ = "1.0.4"
+__all__ = [
+    "config_wizard",
+    "kb_probe",
+    "template_selector",
+    "skill_generator",
+    "installer",
+    "builder_entry"
+]
